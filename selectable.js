151903c43389528c748519e23ce008c571a0a933
Daniel <BurtDaniel@Gmail.com>
2013-03-12 22:13:46 -0700

    Updated images and css

--- selectable.js
+++ selectable.js
@@ -9,3 +9,19 @@ $(function() {
 		}
 	});
 });
+
+jQuery.fn.extend({ 
+        disableSelection : function() { 
+                return this.each(function() { 
+                        this.onselectstart = function() { return false; }; 
+                        this.unselectable = "on"; 
+                        jQuery(this).css('user-select', 'none'); 
+                        jQuery(this).css('-o-user-select', 'none'); 
+                        jQuery(this).css('-moz-user-select', 'none'); 
+                        jQuery(this).css('-khtml-user-select', 'none'); 
+                        jQuery(this).css('-webkit-user-select', 'none'); 
+                }); 
+        } 
+}); 
+
+$("#selectable").disableSelection();
