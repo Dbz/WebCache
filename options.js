3c215471117b9b6f2b322ac273af2c68f8d69cc5
daniel.burt <daniel.burt@workday.com>
2016-12-17 18:12:50 -0800

    Changed to storage sync instead of local Updated manifest version
    Former-commit-id: b3a11edbb697dbe22f33b7071094e2487699dad0

--- options.js
+++ options.js
@@ -1,22 +1,19 @@
 var order = [];
 
-$("#sortable").sortable({
+$('#sortable').sortable({
     stop: function(event, ui) { // Save settings after change
-      // console.log("Old Position: " + order.indexOf(ui.item.attr('id')));
-      // console.log("New position: " + ui.item.index());
-
       order.splice(order.indexOf(ui.item.attr('id')), 1);
       order.splice(ui.item.index(), 0, ui.item.attr('id'));
 
       var saveObj = {};
       saveObj['cacheOrder4'] = order;
-      chrome.storage.local.set(saveObj, function() {
-        console.log("Saved Cache Ordering Preferences");
+      chrome.storage.sync.set(saveObj, function() {
+        console.log('Saved Cache Ordering Preferences');
       });
     },
     create: function(event, ui) { // Set up sortable, checkboxes, & toggle
       // Set up sortable
-      chrome.storage.local.get('cacheOrder4', function(result) {
+      chrome.storage.sync.get('cacheOrder4', function(result) {
         order = result['cacheOrder4'] || ['google-cache-sortable', 'wayback-machine-sortable', 'coral-cdn-sortable'];
 
         var ul = $('#sortable');
@@ -28,13 +25,13 @@ $("#sortable").sortable({
       });
 
       // Set up auto-detect toggle
-      chrome.storage.local.get('auto-detect', function(result) {
+      chrome.storage.sync.get('auto-detect', function(result) {
         toggle = $('#myonoffswitch');
         toggle.prop('checked', result['auto-detect']);
       });
 
       // Set up context menu caches
-      chrome.storage.local.get(['google-cache', 'wayback-machine', 'coral-cdn'], function(results) {
+      chrome.storage.sync.get(['google-cache', 'wayback-machine', 'coral-cdn'], function(results) {
         Object.keys(results).forEach(function(key) {
           $('#' + key).prop('checked', results[key]);
         });
@@ -42,21 +39,21 @@ $("#sortable").sortable({
     }
 });
 
-$("#myonoffswitch").click(function(event) { // Save auto-detect settings
-  chrome.storage.local.set({'auto-detect': $("#myonoffswitch").is(':checked')}, function() {
-    console.log("Saved Auto-Detect Preferences");
+$('#myonoffswitch').click(function(event) { // Save auto-detect settings
+  chrome.storage.sync.set({'auto-detect': $('#myonoffswitch').is(':checked')}, function() {
+    console.log('Saved Auto-Detect Preferences');
   });
 });
 
-$(".context-menu-label").click(function(event) { // Add/Remove context menu caches
+$('.context-menu-label').click(function(event) { // Add/Remove context menu caches
   var id = $(event.target).attr('for');
   var checkbox = $('#' + id);
   var saveObj = {};
   saveObj[id] = !checkbox.is(':checked');
 
-  chrome.storage.local.set(saveObj, function() {
-    console.log("Saved context menu preferences for " + id);
+  chrome.storage.sync.set(saveObj, function() {
+    console.log('Saved context menu preferences for ' + id);
   });
 });
 
-$("#sortable").disableSelection();
+$('#sortable').disableSelection();
