b18fe3f4b1f310fc6e0da80c88557861da93a4de
daniel.burt <daniel.burt@workday.com>
2017-03-05 12:27:57 -0800

    Fixed style issues
    Former-commit-id: 0f88b86accca7436be7ae9fc8d1f010fe3869b7b

--- options.js
+++ options.js
@@ -1,15 +1,8 @@
-const CONTEXT_MENU_CACHES = [
+var CONTEXT_MENU_CACHES = [
   "google-cache",
   "wayback-machine",
   "coral-cdn"
 ];
-
-// Add/Remove context menu caches
-function updateContextMenuCaches(event) {
-  var cache = $(event.target).attr('for');
-  var add_context_menu = !$('#' + cache).is(':checked');
-  add_context_menu ? createContextMenu(cache) : removeContextMenu(cache);
-}
 
 // Remove context menu caches
 function removeContextMenu(cache) {
@@ -24,8 +17,8 @@ function createContextMenu(cache) {
   var saveObj = {};
   var create_properties = {
     id: cache,
-    title: 'Open page with ' + cache.split('-').join(' '), //$("label[for='"+ cache +"']").text()
-    contexts: ['page']
+    title: "Open page with " + cache.split("-").join(" "), //$("label[for=""+ cache +""]").text()
+    contexts: ["page"]
   };
   saveObj[cache] = true;
 
@@ -33,56 +26,63 @@ function createContextMenu(cache) {
   chrome.storage.sync.set(saveObj);
 }
 
+// Add/Remove context menu caches
+function updateContextMenuCaches(event) {
+  var cache = $(event.target).attr("for");
+  var add_context_menu = !$("#" + cache).is(":checked");
+  add_context_menu ? createContextMenu(cache) : removeContextMenu(cache);
+}
+
 var order = [];
-$('#sortable').sortable({
+$("#sortable").sortable({
     stop: function(event, ui) { // Save settings after change
-      order.splice(order.indexOf(ui.item.attr('id')), 1);
-      order.splice(ui.item.index(), 0, ui.item.attr('id'));
+      order.splice(order.indexOf(ui.item.attr("id")), 1);
+      order.splice(ui.item.index(), 0, ui.item.attr("id"));
 
       var saveObj = {};
-      saveObj['cacheOrder4'] = order;
+      saveObj["cacheOrder4"] = order;
       chrome.storage.sync.set(saveObj, function() {
-        console.log('Saved Cache Ordering Preferences');
+        console.log("Saved Cache Ordering Preferences");
       });
     },
 
     // Set up sortable, checkboxes, & toggle
     create: function(event, ui) {
       // Set up sortable
-      chrome.storage.sync.get('cacheOrder4', function(result) {
-        order = result['cacheOrder4'] || ['google-cache-sortable', 'wayback-machine-sortable', 'coral-cdn-sortable'];
+      chrome.storage.sync.get("cacheOrder4", function(result) {
+        order = result["cacheOrder4"] || ["google-cache-sortable", "wayback-machine-sortable", "coral-cdn-sortable"];
 
-        var ul = $('#sortable');
-        var li = ul.children('li').get();
+        var ul = $("#sortable");
+        var li = ul.children("li").get();
         li.sort(function(a, b) {
-          return order.indexOf($(a).attr('id')) - order.indexOf($(b).attr('id'));
+          return order.indexOf($(a).attr("id")) - order.indexOf($(b).attr("id"));
         });
         ul.append(li);
       });
 
       // Set up auto-detect toggle
-      chrome.storage.sync.get('auto-detect', function(result) {
-        toggle = $('#myonoffswitch');
-        toggle.prop('checked', result['auto-detect']);
+      chrome.storage.sync.get("auto-detect", function(result) {
+        var toggle = $("#myonoffswitch");
+        toggle.prop("checked", result["auto-detect"]);
       });
 
       // Set up context menu caches
       chrome.storage.sync.get(CONTEXT_MENU_CACHES, function(results) {
         Object.keys(results).forEach(function(key) {
-          $('#' + key).prop('checked', !!results[key]);
+          $("#" + key).prop("checked", !!results[key]);
         });
       });
     }
 });
 
 // Save auto-detect settings
-$('#myonoffswitch').click(function(event) {
-  chrome.storage.sync.set({'auto-detect': $('#myonoffswitch').is(':checked')}, function() {
-    console.log('Saved Auto-Detect Preferences');
+$("#myonoffswitch").click(function(event) {
+  chrome.storage.sync.set({"auto-detect": $("#myonoffswitch").is(":checked")}, function() {
+    console.log("Saved Auto-Detect Preferences");
   });
 });
 
 // Add/Remove context menu caches
-$('.context-menu-label').click(updateContextMenuCaches);
+$(".context-menu-label").click(updateContextMenuCaches);
 
-$('#sortable').disableSelection();
+$("#sortable").disableSelection();
--- redirect.js
+++ redirect.js
@@ -1,19 +1,19 @@
-var currentURL        = '';
+var currentURL        = "";
 var isHTTPS           = true;
 var index             = 0;
 var numberOfRedirects = 0;
 var redirecting       = false;
 
 var cacheURL = [
-  'http://webcache.googleusercontent.com/search?q=cache:',
-  'http://web.archive.org/web/*/',
-  '.nyud.net'
+  "http://webcache.googleusercontent.com/search?q=cache:",
+  "http://web.archive.org/web/*/",
+  ".nyud.net"
 ];
-var cacheNames = ['google-cache-sortable', 'wayback-machine-sortable', 'coral-cdn-sortable'];
+var cacheNames = ["google-cache-sortable", "wayback-machine-sortable", "coral-cdn-sortable"];
 var URL_HASH = [
-  { name: 'google-cache-sortable', URL: 'http://webcache.googleusercontent.com/search?q=cache:' },
-  { name: 'wayback-machine-sortable', URL: 'http://web.archive.org/web/*/' },
-  { name: 'coral-cdn-sortable', URL: '.nyud.net' }
+  { name: "google-cache-sortable", URL: "http://webcache.googleusercontent.com/search?q=cache:" },
+  { name: "wayback-machine-sortable", URL: "http://web.archive.org/web/*/" },
+  { name: "coral-cdn-sortable", URL: ".nyud.net" }
 ];
 
 function getURL() {
@@ -21,29 +21,29 @@ function getURL() {
   if(cacheURL[index]  == cacheURL.length) {
     index = 0;
   }
-  if(cacheURL[index] != '.nyud.net') { // Google and Wayback Machine
-    return cacheURL[index] + (isHTTPS ? currentURL.substr(8) : currentURL.substr(7))
+  if(cacheURL[index] != ".nyud.net") { // Google and Wayback Machine
+    return cacheURL[index] + (isHTTPS ? currentURL.substr(8) : currentURL.substr(7));
   } else { // Coral CDN
-    if(currentURL.slice(-1) == '/') {
-      return currentURL.substring(0, currentURL.length -1) + '.nyud.net';
+    if(currentURL.slice(-1) == "/") {
+      return currentURL.substring(0, currentURL.length -1) + ".nyud.net";
     } else {
-      return currentURL + '.nyud.net';
+      return currentURL + ".nyud.net";
     }
   }
 }
 
 function handler(details) { 
-  //console.log('StatusLine is: ' + details.statusLine);
+  //console.log("StatusLine is: " + details.statusLine);
   if(numberOfRedirects > cacheURL.length) { // There is no cache available
     chrome.webRequest.onHeadersReceived.removeListener(handler);
     redirecting = false;
     return { cancel: true };
   }
-  if(~details.statusLine.search('404')) { // Not found
+  if(~details.statusLine.search("404")) { // Not found
     ++index;
     return { redirectUrl:getURL() };
   }
-  else if(~details.statusLine.search('403')) { // Forbidden
+  else if(~details.statusLine.search("403")) { // Forbidden
     ++index;
     return { redirectUrl:getURL() };
   }
@@ -55,8 +55,8 @@ function handler(details) {
 }
 
 function openPage(currentTab) {
-  chrome.storage.sync.get('cacheOrder4', function(result) {
-    var cacheOrder = result['cacheOrder4'] || cacheNames;
+  chrome.storage.sync.get("cacheOrder4", function(result) {
+    var cacheOrder = result["cacheOrder4"] || cacheNames;
         
     URL_HASH.sort(function(a, b) {
       return cacheOrder.indexOf(a.name) - cacheOrder.indexOf(b.name);
@@ -76,17 +76,17 @@ function openPage(currentTab) {
     chrome.webRequest.onHeadersReceived.addListener(
       handler,
       {
-        urls: ['<all_urls>'],
-        types: ['main_frame']
+        urls: ["<all_urls>"],
+        types: ["main_frame"]
       },
-      ['blocking']
+      ["blocking"]
     );
     
     currentURL = currentTab.url;
 
-    isHTTPS = (currentURL.substring(0, 6) == 'https:');
+    isHTTPS = (currentURL.substring(0, 6) == "https:");
 
-    //console.log('the tab id for the redirect is ' + currentTab.id + ': The website is: ' + currentURL);
+    //console.log("the tab id for the redirect is " + currentTab.id + ": The website is: " + currentURL);
       
     chrome.tabs.update(currentTab.id, { url: getURL(index) });
         
@@ -106,37 +106,37 @@ function autoRedirect(details) {
     if(tab == null || redirecting)
       return;
     
-    if(~details.statusLine.indexOf('408')) {
-      console.log('408 redirect');
-    } else if(~details.statusLine.indexOf('503')) {
-      console.log('503 redirect');
+    if(~details.statusLine.indexOf("408")) {
+      console.log("408 redirect");
+    } else if(~details.statusLine.indexOf("503")) {
+      console.log("503 redirect");
       openPage(tab);
-    } else if(~details.statusLine.indexOf('521')) {
-      console.log('521 redirect');
+    } else if(~details.statusLine.indexOf("521")) {
+      console.log("521 redirect");
       openPage(tab);
-    } else if(~details.statusLine.indexOf('522')) {
-      console.log('522 redirect');
+    } else if(~details.statusLine.indexOf("522")) {
+      console.log("522 redirect");
       openPage(tab);
     }
   });
 }
 
-chrome.storage.sync.get('auto-detect', function(result) {
-  if(result['auto-detect'] == 'on') {
+chrome.storage.sync.get("auto-detect", function(result) {
+  if(result["auto-detect"] == "on") {
     chrome.webRequest.onHeadersReceived.addListener(
       autoRedirect,
-      { urls: ['<all_urls>'] }
+      { urls: ["<all_urls>"] }
     );
   }
 });
 
 // Set defaults on install or update
 chrome.runtime.onInstalled.addListener(function(details) {
-  chrome.storage.sync.get('cacheOrder', function(result) {
+  chrome.storage.sync.get("cacheOrder", function(result) {
     if(Object.keys(result).length == 0) {
       var saveObject = {
-        'cacheOrder4': cacheNames,
-        'auto-detect': 'off'
+        "cacheOrder4": cacheNames,
+        "auto-detect": "off"
       };
       chrome.storage.sync.set(saveObject);
     }
